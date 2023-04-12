d677c3c451f897496283f0d546b183369ebd5b1f
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-04-13 03:41:43 +0500

    auth js with existing email validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,39 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router();
 // const user = User
 const { body, validationResult } = require('express-validator');
 
-// creating a user
-router.post('/',[
+// creating a user at /api/auth//createUser
+router.post('/createUser',[
     body('name','name is not defined').isLength({ min: 3 }),
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
-] ,(req, res) => { 
+] ,async (req, res) => { 
     // console.log(req.body)
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
-    User.create({
+    try{
+    // Checking wheather email already being registered
+    let user = await User.findOne({email: req.body.email})
+    if(user){
+        return res.status(400).json({error:"bhaiii kia kr rha hy ye email pehle se hi registered hy naya email daalo ya mun utha kr purane waly se sign in kroo"})
+    }
+    // creating new user
+     user = await Ufser.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user)).catch(error=>console.log(error))
+        
+      })
+      console.log('usercreated')
+      res.json(user)}catch(error){
+        res.status(500).send("some error occured")
+      }
 })
 
 module.exports = router ;
