10d60d29431af136976e75805f3a658fa237cdc7
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-06-26 00:38:33 +0500

    last of today

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-// app.listen(port, () => {
-//   console.log(`INotebook app listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`INotebook app listening on port ${port}`);
+});
  
 module.exports = app;
