447a1d399850c71be7f4f3271909d2f4a8088d3e
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-06-26 00:34:47 +0500

    commented app.listen

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 var cors = require('cors')
 connectToMongo()
 const app = express()
-const port = 5000;
+// const port = 5000;
 app.use(cors())
 
 app.use(express.json())
@@ -13,8 +13,8 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.listen(port, () => {
-  console.log(`INotebook app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`INotebook app listening on port ${port}`);
+// });
  
 module.exports = app;
