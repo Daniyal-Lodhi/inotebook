af45bae9b7a6d3828683c8d909fd78cbe3ae87ec
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-06-30 15:30:36 +0500

    removed backend

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb+srv://daniyallodhi25:daniyal123@cluster0.eczrsa5.mongodb.net/'
-
-const connectToMongo = async()=>{
-        mongoose.connect(URI)
-        console.log("connected to mongo successfully")
-
-}
-module.exports = connectToMongo;  
